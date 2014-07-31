9e174edd562f6f1797bd1fd52e4e603f00ebddd7
Robert Rothenberg <rrwo@cpan.org>
2014-07-31 21:55:39 +0100

    Added prototype module

---
+++ lib/Struct/Flatten/Template.pm
@@ -0,0 +1,59 @@
+package Struct::Flatten::Template;
+
+use Moose;
+
+has 'callback' => (
+    is  => 'ro',
+    isa => 'Maybe[CodeRef]',
+);
+
+has 'template' => (
+    is => 'ro',
+    isa => 'Ref',
+);
+
+sub process {
+    my ($self, @args) = @_;
+
+    my $struct = $args[0];
+    my $template = $#args ? $args[1] : $self->template;
+
+    if (my $type = (ref $template)) {
+
+        if (my $fn = $self->callback) {
+
+            $fn->($self, $struct, $template)
+                if (($type eq 'REF') &&
+                    (ref( ${$template} ) eq 'HASH'));
+
+        }
+
+        return if ($type ne ref($struct));
+
+        my $method = "process_${type}";
+        $method =~ s/::/_/g;
+
+        if (my $fn = $self->can($method)) {
+            $self->$fn($struct, $template);
+        }
+    }
+ }
+
+
+sub process_HASH {
+    my ($self, $struct, $template) = @_;
+
+    foreach my $key (keys %{$template}) {
+        $self->process( $struct->{$key}, $template->{$key},  )
+            if exists $struct->{$key};
+    }
+}
+
+sub process_ARRAY {
+    my ($self, $struct, $template) = @_;
+    $self->process( $_, $template->[0] )
+        for @{$struct};
+}
+
+
+1;
